4300349215c81938be433d2b4e5d65b5f98e2940
Marcin Cyboran <marcin.cyboran@testarmy.com>
2020-02-03 11:35:56 +0100

    Push commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,77 +3,77 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        // polyfills: '@babel/polyfill',
-        app: './src/ts/app.ts'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].bundle.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                include: path.resolve(__dirname, 'src/ts'),
-                exclude: /node_modules/,
-                use: 'ts-loader'
-            },
-            {
-                test: /\.scss$/i,
-                include: [path.resolve(__dirname, 'src/sass')],
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../'
-                        }
-                    },
-                    'css-loader',
-                    'resolve-url-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif|ico)$/,
-                include: [path.resolve(__dirname, 'src/assets')],
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'assets'
-                        }
-                    }
-                ]
+  mode: 'development',
+  entry: {
+    // polyfills: '@babel/polyfill',
+    app: './src/ts/app.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: path.resolve(__dirname, 'src/ts'),
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.scss$/i,
+        include: [path.resolve(__dirname, 'src/sass')],
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
             }
+          },
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
         ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'style/[name].css',
-            chunkFilename: 'style/[id].css'
-        }),
-        new HTMLWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        })
+      },
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        include: [path.resolve(__dirname, 'src/assets')],
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets'
+            }
+          }
+        ]
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style/[name].css',
+      chunkFilename: 'style/[id].css'
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
 }
